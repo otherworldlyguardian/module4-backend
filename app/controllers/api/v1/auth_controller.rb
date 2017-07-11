b76e50af6f47cee4c06c8f60b3cc0a959cc98c71
sammygee140 <samuelzgoldsmith@gmail.com>
2017-07-11 17:11:54 -0400

    finished backend auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -11,13 +11,15 @@ module Api
 
       def create
         user = User.find_by(username: params[:username])
-        if user.present? && user.authenticate(params[:password])
+        if user && user.authenticate(params[:password])
           render json: {
             username: user.username,
             jwt: issue_token ({id: user.id})
           }
         else
-
+          render json: {
+            error: 'Incorrect information'
+          }, status: 401
         end
       end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,38 @@
 class ApplicationController < ActionController::API
+  private
 
   def issue_token payload
     JWT.encode payload, hmac_secret, 'HS256'
   end
 
   def authorize_user!
-    if !current_user.present?
+    if !current_user
       render json: {error: 'error'}
     end
   end
 
   def current_user
-    @current_user ||= User.find_by(id: token_user_id)
+    @current_user ||= User.find(token_user_id)
   end
 
   def token_user_id
-    decoded_token.first['id']
+    decoded_token.first['user_id']
   end
 
   def decoded_token
     if token
       begin
         JWT.decode token, hmac_secret, true, {:algorithm => 'HS256'}
-      rescue
+      rescue JWT::DecodeError
       [{}]
       end
     else
       [{}]
     end
+  end
+
+  def token
+    request.headers['Authorization']
   end
 
 end
