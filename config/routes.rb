4173223c0c7d9a473f75d238e2a8e3e0d7209cbf
sammygee140 <samuelzgoldsmith@gmail.com>
2017-07-11 12:37:57 -0400

    added users table

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      post '/login' to: 'auth#create'
-      post '/me' to: 'auth#show'
+      post '/login', to: 'auth#create'
+      post '/me', to: 'auth#show'
     end
   end
 end
